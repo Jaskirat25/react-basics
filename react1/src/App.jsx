3a94901a66386dd9ca0aaad6798938de676222e4
jaskirat <jaskiratsingh251103@gmail.com>
2024-12-14 22:59:07 +0530

    first commit

---
+++ react1/src/App.jsx
@@ -0,0 +1,10 @@
+import Hello from './index'
+
+const App = () => {
+  return (
+    <Hello/>
+  )
+}
+
+export default App
+
---
+++ react1/src/index.jsx
@@ -0,0 +1,6 @@
+function Hello(){
+    return (
+        <h1>hello world</h1>
+    )
+}
+export default Hello
