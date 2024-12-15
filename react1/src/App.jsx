652ed988e45d659c199f35dad880e6632535d2f0
jaskirat <jaskiratsingh251103@gmail.com>
2024-12-15 23:55:18 +0530

    increment function

--- react1/src/App.jsx
+++ react1/src/App.jsx
@@ -1,8 +1,30 @@
 import Hello from './index'
+import { useState } from 'react'
 
 const App = () => {
+  let [counter,set] =useState(0)
+  const inc=()=>{
+    if(counter<20){
+  set(counter+1)
+    }
+  }
+  const dec=()=>{
+    if(counter>0){
+  set(counter-1)
+    }
+  }
   return (
-    <Hello/>
+   <div>
+    <h1>
+      helloooo
+    </h1>
+<br />
+<p>counter:{counter}</p>
+    <button onClick={inc}>up</button>
+    <br />
+    <button onClick={dec}>down</button>
+
+   </div>
   )
 }
 
